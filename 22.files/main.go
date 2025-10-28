f70104c6324f0837c25184a74354100d66c07b46
sachinpoudel <poudelsachin009@gmail.com>
2025-10-28 21:54:33 +0545

    Implement file reading functionality and add example.txt

--- 22.files/main.go
+++ 22.files/main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func main(){
+f, err := os.Open("example.txt")
+ if err != nil {
+	panic(err)
+ }
+// fileInfo , err := f.Stat()
+// if err != nil {
+// 	panicrr)
+// }
+
+// fmt.Println("file name", fileInfo.Name())
+// fmt.Println("file name", fileInfo.IsDir())
+// fmt.Println("file name", fileInfo.Size())
+// fmt.Println("file modified at", fileInfo.ModTime())
+
+defer f.Close()
+
+// buf:= make([]byte, 10)
+// d, err:= f.Read(buf)
+
+// if err != nil {
+// 	panic(err)
+// }
+
+data , err := os.ReadFile("example.txt")
+
+if err != nil {
+	panic(err)
+}
+
+fmt.Println(string(data))
+
+
+
+
+
+}
